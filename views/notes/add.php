478c10dde33d8b0f4ef41ec1ddd016088efe3e39
Mrbilal <bilal_itchir@Outlook.fr>
2023-12-22 20:25:18 +0100

    🐛 corriger(add.php) : afficher une chaîne vide si $pageTitle est vide pour éviter une erreur ✨ fonctionnalité(add.php) : inclure le formulaire de note à partir d'un composant externe pour améliorer la modularité du code

--- views/notes/add.php
+++ views/notes/add.php
@@ -1,15 +1,4 @@
-<h1><?= $pageTitle ?></h1>
+<h1><?= !empty($pageTitle) ? $pageTitle : '' ?></h1>
+<?php
 
-<a href="/notes" class="btn btn-secondary mb-3" role="button" aria-pressed="true">Notes list</a>
-
-<form action="" method="post">
-  <div class="form-group mb-2">
-    <label for="title" class="form-label">Titre de la note</label>
-    <input type="text" name="title" id="title" class="form-control">
-  </div>
-  <div class="form-group mb-2">
-    <label for="content" class="form-label">Contenu de la note</label>
-    <textarea name="content" id="content" cols="30" rows="10" class="form-control"></textarea>
-  </div>
-  <input type="submit" name="submit" value="Enregister" class="btn btn-success">
-</form>
+include __DIR__ . '/../components/note_form.php';
