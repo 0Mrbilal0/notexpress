f3b05063379915bd03f47c78878c91758ad7c519
Mrbilal <bilal_itchir@Outlook.fr>
2023-12-22 20:26:11 +0100

    🔧 corriger(edit.php) : supprimer le code redondant et utiliser le composant note_form.php pour afficher le formulaire de modification de note

--- views/notes/edit.php
+++ views/notes/edit.php
@@ -1,22 +1,4 @@
+<h1><?= !empty($pageTitle) ? $pageTitle : '' ?></h1>
 <?php
 
-require_once __DIR__ . '/../../src/actions/getNote.php';
-
-$pageTitle = 'Edit note';
-
-?>
-
-<h1><?= $pageTitle ?></h1>
-
-<form action="/note/edit" method="post">
-  <div class="form-group mb-2">
-    <label for="title" class="form-label">Titre de la note</label>
-    <input type="text" name="title" id="title" class="form-control" value="<?= $note->getTitle() ?>">
-  </div>
-  <div class="form-group mb-2">
-    <label for="content" class="form-label">Contenu de la note</label>
-    <textarea name="content" id="content" cols="30" rows="10" class="form-control"><?= $note->getContent() ?></textarea>
-  </div>
-  <input type="hidden" id="slug" name="slug" value="<?= $note->getSlug() ?>">
-  <input type="submit" name="submit" value="Enregister" class="btn btn-success">
-</form>
+include __DIR__ . '/../components/note_form.php';
