8a75b5f74b3716697e2a214802e51455ba061787
Mrbilal <bilal_itchir@Outlook.fr>
2023-12-22 20:26:24 +0100

    🐛 corriger(show.php) : supprimer le code inutile et réorganiser la structure HTML ✨ fonctionnalité(show.php) : ajouter une image de fond à la div principale et afficher le titre de la note dans un élément h1 distinct

--- views/notes/show.php
+++ views/notes/show.php
@@ -1,24 +1,13 @@
-<?php
-
-require_once __DIR__ . '/../../src/actions/getNote.php';
-
-$pageTitle = 'Note';
-?>
-
-<h1><?= $pageTitle ?></h1>
-
-<div class="list-group">
-  <div class="d-flex w-100 justify-content-between">
-    <h5 class="mb-1"><?= $note->getTitle(); ?></h5>
-  </div>
-  <p class="mb-1"><?= $note->getContent(); ?></p>
-</div>
-<div class="d-flex align-items-center gap-2">
-  <a href="/note/edit?slug=<?= $note->getSlug(); ?>&edit=Edit" class="btn btn-primary">EDIT</a>
-  <form action="/note/delete" method="POST">
-    <input type="hidden" name="slug" value="<?= $note->getSlug(); ?>">
-    <input class="btn btn-danger" type="submit" name="delete" value="Delete">
-  </form>
+<div class="p-5 text-center bg-body-tertiary rounded-3" style="
+        background-image: url(/assets/images/uploads/<?= !empty($note->getImage()) ? $note->getImage() : 'default.png' ?>);
+        background-size: cover;
+        background-position: center;
+        ">
+    <h1 class="text-body-emphasis p-3 rounded-3" style="background-color: antiquewhite"><?= !empty($pageTitle) ? $pageTitle : '' ?></h1>
 </div>
 
-<?php
+<p class="border rounded p-3 m-3">
+    <?= $note->getContent(); ?>
+</p>
+
+<a href="/note/edit?slug=<?= $note->getSlug(); ?>" class="btn btn-secondary">Modifier</a>
